c64fee8c61145eb446123cdb3602076a034b89c9
Bharathi978 <sharathibharathi@gmail.com>
2023-05-09 10:13:05 +0530

    verify

---
+++ src/org/in/Google.java
@@ -0,0 +1,16 @@
+package org.in;
+
+public class Google {
+	public void phoneNumber() {
+System.out.println("12345678");
+	}
+public static void main(String[] args) {
+	Google g = new Google();
+	g.phoneNumber();
+	Telegram k = new Telegram();
+	k.methodName();
+	k.keyWord();
+
+	
+}
+}
---
+++ src/org/in/Telegram.java
@@ -0,0 +1,12 @@
+package org.in;
+
+public class Telegram {
+public void methodName() {
+System.out.println("user");
+}
+public void keyWord() {
+System.out.println("123456");
+}
+	
+
+}
